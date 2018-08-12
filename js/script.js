c379fdb95870e7b973bee7e2bb94c6483f244263
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-12 16:44:26 -0600

    Added function for color change; not yet working

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function turnGreen() {
+	let voyages = document.getElementById("voyages");
+	voyages.style.color = "green";
+}
+
+function playfairFont() {
+	let voyagesParagraph = document.getElementById("voyages");
+	voyagesParagraph.font = style.css("playfairFont");
+}
