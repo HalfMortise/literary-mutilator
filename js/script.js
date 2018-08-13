d70673073c115638840a9fcf166d834320bc5952
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-13 14:19:48 -0600

    Added 'highlight word' function

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ function changeFont() {
 	let voyages = document.getElementById("voyages");
 	voyages.style.fontFamily = ("Courier New");
 }
+
+
+document.addEventListener('DOMContentLoaded', function(){
+	let searchpara = document.getElementById("search_para").innerHTML;
+	searchpara=searchpara.toString();
+	document.getElementById("search").onclick = function ()
+	{highlight_word(searchpara)};
+},false);
+
+function highlight_word(searchpara) {
+	let text=document.getElementById("search_text").value;
+	if(text) {
+		let pattern=new RegExp("("+text+")", "gi");
+		let new_text=searchpara.replace(pattern, "<span class='highlight'>"+text+"</span>");
+		document.getElementById("search_para").innerHTML=new_text;
+	}
+}
